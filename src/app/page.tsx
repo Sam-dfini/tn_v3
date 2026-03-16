f29af2beefc10bad07aeb3e4ae28bc50fba8641f
Sam-dfini <samir.dfini@gmail.com>
2026-03-16 07:57:16 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useLang } from '@/lib/LangContext';
 import SemiGauge from '@/components/gauges/SemiGauge';
 import SensorGrid from '@/components/SensorGrid'; // adjust path if needed
-mport TunisiaMap from '@/components/TunisiaMap';
+import TunisiaMap from '@/components/TunisiaMap';
 
 {/* Inside your dashboard layout */}
 <div className="flex-1 relative h-full">
