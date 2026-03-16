3506c87742bdee4905a68d77c291a7153e6ebfc5
Sam-dfini <samir.dfini@gmail.com>
2026-03-16 08:15:22 +0000

    Update TunisiaMap.tsx

--- src/components/TunisiaMap.tsx
+++ src/components/TunisiaMap.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'; // ← this line is now valid
 
-// Dynamic import to avoid SSR issues
 const MapContainer = dynamic(
   () => import('react-leaflet').then((mod) => mod.MapContainer),
   { ssr: false }
@@ -15,21 +14,16 @@ const TileLayer = dynamic(
 
 export default function TunisiaMap() {
   return (
-    <div className="w-full h-full relative">
-      <MapContainer
-        center={[34.0, 9.5]} 
-        zoom={7} 
-        className="w-full h-full"
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; OpenStreetMap contributors'
-        />
-        
-        {/* Your existing event markers, overlays, etc. go here */}
-        {/* For now this will at least show the map tiles */}
-      </MapContainer>
-    </div>
+    <MapContainer
+      center={[34.0, 9.5]}
+      zoom={7}
+      style={{ height: '100%', width: '100%' }}
+      className="bg-black"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap contributors'
+      />
+    </MapContainer>
   );
 }
